ce240bad214e861ffb17392ba3b41cf6a5ddfd67
Ryan William O'Hara <ryan@ryanwohara.com>
2022-04-07 14:49:15 -0600

    Load channels from the env file

--- main.py
+++ main.py
@@ -9,23 +9,12 @@ import os
 
 load_dotenv()
 
-channels = {
-    '#rshelp': 306269746280529920,
-    '#bullshit': 328270978524381184,
-    '#minecraft': 576528017661231124,
-    '#Games': 882666320406138920,
-    '#programming': 882384979609403402,
-    '#swiftirc': 875742213978595378,
-    '#cooking': 882385109343416340
-    # '#asdfghj': 306269746280529920
-}
-
 config = {
     'TOKEN': os.getenv('TOKEN'),
     'NICK': os.getenv('NICK'),
     'SERVER': os.getenv('SERVER'),
     'PORT': int(os.getenv('PORT')),
-    'CHANNELS': channels
+    'CHANNELS': json.loads(os.getenv('CHANNELS'))
 }
 
 thread_lock = threading.Lock()
